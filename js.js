69cca7f871c3a631c1488d6ec65794f08a750dd2
Robert <wyb4wiciel@gmail.com>
2020-09-19 19:48:30 +0200

    take data from new Date

--- js.js
+++ js.js
@@ -2,10 +2,21 @@ let clockS = document.querySelector('.clock__up--secondHand');
 let clockM = document.querySelector('.clock__up--minuteHand');
 let clockH = document.querySelector('.clock__up--hourHand');
 
-let secondNum = 0;
-let minuteNum = 0;
-let hourNum = 0;
+//Download time from new Date and put into css
+setInterval(() => {
+    const time = new Date();
+    let secondNum = time.getSeconds() * 6;
+    let minuteNum = time.getMinutes() * 6;
+    let hourNum = time.getHours() * 12;
+    clockS.style.transform = "rotate("+ secondNum +"deg)";
+    clockM.style.transform = "rotate("+ minuteNum +"deg)";
+    clockH.style.transform = "rotate("+ hourNum+"deg)";
+    console.log();
 
+}, 50);
+
+
+/* //adding time " by hand " withouy new Date
 function seconds() {
     clockS.style.transform = "rotate("+ secondNum +"deg)";
 }
@@ -14,14 +25,14 @@ function minutes() {
     if (minuteNum % 360 == 0 ) {
         hourNum += 15;
         hours()
-        console.log(hourNum);
+       // console.log(hourNum);
     }
 }
 function hours() {
     clockH.style.transform = "rotate("+ hourNum +"deg)";
 }
-
-setInterval(() => {
+*/
+/* setInterval(() => {
     secondNum += 6;
     //console.log(secondNum);
     seconds();
@@ -29,7 +40,23 @@ setInterval(() => {
     if (secondNum % 360 == 0) {
         minuteNum += 6;
         minutes();
-        console.log(minuteNum);
+        //console.log(minuteNum);
     }    
 }, 1000);
+*/
 
+
+/*
+const clock = () => {
+    const time = new Date();
+    // console.log(time.toLocaleString());
+    // console.log(time.toLocaleDateString());
+    const secondsB = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+    const minutesB = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+    const hoursB = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+   console.log(time.getSeconds());
+    //document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`
+   }
+   
+   setInterval(clock, 1000);
+   */
