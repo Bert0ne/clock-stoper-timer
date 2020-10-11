6dd2c3304faa6c4bf81e6c89e5158872157a4491
Robert <wyb4wiciel@gmail.com>
2020-10-11 13:06:54 +0200

    make modal forms

--- js.js
+++ js.js
@@ -120,6 +120,8 @@
 
   function run() {
     stopWatchEl.textContent = getTime();
+    document.title = getTime();
+
     miliSecForLapTime++;
     ms++;
     if(ms == 100) {
@@ -181,7 +183,52 @@ function lapCount(data) {
   let ms = (dummy % 100);
   let s = Math.floor(dummy / 100);
   let m = Math.floor(s / 60)
-  console.log(dummy);
+  console.log(dummDy);
 
   return (m < 10 ? "0" + m : m) +  ":" + (s < 10 ? "0" + s : s) + ":" + (ms < 10 ? "0" + ms : ms);
   */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
+  const endTime = new Date('2019-01-05 18:34:00').getTime();
+
+const spanD = document.querySelector('span.d');
+const spanH = document.querySelector('span.h');
+const spanM = document.querySelector('span.m');
+const spanS = document.querySelector('span.s');
+
+setInterval(() => {
+ const nowTime = new Date().getTime();
+ // const time = Math.floor((endTime - nowTime) / 1000);
+ const time = endTime - nowTime;
+ const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+ console.log(days);
+
+ let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+ // Przykład - dodanie 0 przeg godziną 
+ hours = hours < 10 ? `0${hours}` : hours;
+
+ const minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+
+ const secs = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+
+ spanD.textContent = days;
+ spanH.textContent = hours;
+ spanM.textContent = minutes;
+ spanS.textContent = secs;
+}, 1000)
+
+
+*/
